10a9509beedd75b903ae9a082e50ae2412271466
catalinmn <nan.catalin@yahoo.com>
2021-02-27 14:21:03 +0200

    adaugare clase ZooKeeper si Animal

---
+++ CTS-Seminar1/src/classes/ZooKeeper.java
@@ -0,0 +1,26 @@
+package classes;
+
+public class ZooKeeper {
+	
+	private String nume;
+
+	public ZooKeeper(String nume) {
+		super();
+		this.nume = nume;
+	}
+	
+	public void feed(Animal a) {
+		System.out.println("Zookeeper " + this.nume + " feeds " + a.getNume());
+	}
+
+	public String getNume() {
+		return nume;
+	}
+
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+	
+	
+
+}
