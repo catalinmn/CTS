10900f49952b3e282b394155cd5abcc949bedcf9
catalinmn <nan.catalin@yahoo.com>
2021-05-29 14:58:39 +0300

    commit seminar 12 JUNIT PART 2

---
+++ CTS-Seminar12/src/ro/ase/cts/teste/GrupaFixtureTests.java
@@ -0,0 +1,31 @@
+package ro.ase.cts.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ro.ase.cts.clase.Grupa;
+import ro.ase.cts.clase.Student;
+
+public class GrupaFixtureTests {
+
+private Grupa grupa;
+	
+	@Before
+	public void setup() {
+		this.grupa = new Grupa(1078);
+		for (int i =0; i<35; i++) {
+			Student student = new Student();
+			for (int j =0; j<7; j++) {		
+				student.adaugaNota(9);		
+			}
+			grupa.adaugaStudent(student);
+		}
+	}
+
+	@Test(timeout=500)
+	public void testGetPromovabilitatePerformance() {
+		grupa.getPromovabilitate();
+	}
+}
---
+++ CTS-Seminar12/src/ro/ase/cts/teste/TestGrupa.java
@@ -0,0 +1,134 @@
+package ro.ase.cts.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ro.ase.cts.clase.Grupa;
+import ro.ase.cts.clase.Student;
+
+public class TestGrupa {
+	
+
+	@Test
+	public void testConstructorRight() {
+		Grupa grupa = new Grupa(1078);
+		assertEquals(1078, grupa.getNrGrupa());
+	}
+	
+	@Test
+	public void testConstructorLimitaInferioara() {
+		Grupa grupa = new Grupa (1000);
+		
+		assertEquals(1000, grupa.getNrGrupa());
+	}
+	
+	@Test
+	public void testConstructorLimitaSuperioara() {
+		Grupa grupa = new Grupa (1100);
+		
+		assertEquals(1100, grupa.getNrGrupa());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testConstructorSubLimita() {
+		Grupa grupa = new Grupa (999);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testConstructorPesteLimita() {
+		Grupa grupa = new Grupa (1110);
+	}
+	
+	@Test(timeout=200)
+	public void testConstructorPerformance() {
+		Grupa grupa = new Grupa(1078);
+	}
+	
+	@Test
+	public void testPromovabilitateRight() {
+		Grupa grupa = new Grupa(1078);
+		for (int i =0; i<8; i++) {
+			Student student = new Student();
+			student.adaugaNota(10);
+			grupa.adaugaStudent(student);
+		}
+		for (int i =0; i<2; i++) {
+			Student student = new Student();
+			student.adaugaNota(4);
+			grupa.adaugaStudent(student);
+		}
+		
+		assertEquals(0.8, grupa.getPromovabilitate(), 0.001);
+	}
+	
+	@Test
+	public void testGetPromovabilitateRight() {
+		Grupa grupa = new Grupa(1078);
+		for (int i =0; i<8; i++) {
+			Student student = new Student();
+			student.adaugaNota(10);
+			grupa.adaugaStudent(student);
+		}
+		
+		assertEquals(1, grupa.getPromovabilitate(), 0.001);
+	}
+
+	@Test
+	public void testGetPromovabilitateLeft() {
+		Grupa grupa = new Grupa(1078);
+		for (int i =0; i<8; i++) {
+			Student student = new Student();
+			student.adaugaNota(4);
+			grupa.adaugaStudent(student);
+		}
+		
+		assertEquals(0, grupa.getPromovabilitate(), 0.001);
+	}
+	
+	@Test
+	public void testGetPromovabilitateInverse() {
+		int nrIntegralisti = 31;
+		int nrRestantieri = 2;
+		Grupa grupa = new Grupa(1078);
+		for (int i =0; i<nrIntegralisti; i++) {
+			Student student = new Student();
+			student.adaugaNota(10);
+			grupa.adaugaStudent(student);
+		}
+		for (int i =0; i<nrRestantieri; i++) {
+			Student student = new Student();
+			student.adaugaNota(4);
+			grupa.adaugaStudent(student);
+		}
+		
+		assertEquals(nrIntegralisti, grupa.getPromovabilitate() * grupa.getStudenti().size(), 0.001);
+	}
+	
+	@Test
+	public void testGetPromovabilitateError() {
+		Grupa grupa = new Grupa(1078);
+		
+		assertEquals(0, grupa.getPromovabilitate(), 0.001);
+	}
+	
+	@Test
+	public void testGetPromovabilitateCardinalityAreRestanta() {
+		Grupa grupa = new Grupa(1078);
+		Student student = new Student();
+		student.adaugaNota(4);
+		grupa.adaugaStudent(student);
+		assertEquals(0, grupa.getPromovabilitate(), 0.001);
+	}
+	
+	@Test
+	public void testGetPromovabilitateCardinalityNuAreRestanta() {
+		Grupa grupa = new Grupa(1078);
+		Student student = new Student();
+		student.adaugaNota(8);
+		grupa.adaugaStudent(student);
+		assertEquals(1, grupa.getPromovabilitate(), 0.001);
+	}
+	
+}
