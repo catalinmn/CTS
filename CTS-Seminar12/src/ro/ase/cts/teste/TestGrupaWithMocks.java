c54643519db0b3f44d7a1b7fcb979d7ec446b7f2
catalinmn <nan.catalin@yahoo.com>
2021-06-05 14:21:48 +0300

    ultimul seminar - mock tests

---
+++ CTS-Seminar12/src/ro/ase/cts/teste/TestGrupaWithMocks.java
@@ -0,0 +1,60 @@
+package ro.ase.cts.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ro.ase.cts.clase.Grupa;
+import ro.ase.cts.clase.Student;
+import ro.ase.cts.mocks.StudentDummy;
+import ro.ase.cts.mocks.StudentFake;
+import ro.ase.cts.mocks.StudentStub;
+
+public class TestGrupaWithMocks {
+
+	@Test
+	public void testAdaugareStudent() {
+		
+		StudentDummy student = new StudentDummy();
+		Grupa grupa = new Grupa(1100);
+		
+		grupa.adaugaStudent(student);
+		
+		assertEquals(1, grupa.getStudenti().size());
+	}
+
+	@Test
+	public void testGetPromovabilitate() {
+		
+		StudentStub student = new StudentStub();
+		Grupa grupa = new Grupa(1100);
+		
+		grupa.adaugaStudent(student);
+		
+		assertEquals(0, grupa.getPromovabilitate(), 0.01);
+	}
+	
+	@Test
+	public void testGrupaWithFakeRight() {
+	
+		// 10 student - 7 integralisti + 3 restantieri
+		
+		int nrIntegralisti = 7;
+		int nrRestantieri = 3;
+		Grupa grupa = new Grupa(1078);
+		for (int i =0; i<nrIntegralisti; i++) {
+			StudentFake student = new StudentFake();
+			student.setValoareAreRestanta(false);
+			grupa.adaugaStudent(student);
+		}
+		for (int i =0; i<nrRestantieri; i++) {
+			StudentFake student = new StudentFake();
+			student.setValoareAreRestanta(true);
+			grupa.adaugaStudent(student);
+		}
+		
+		assertEquals(0.7, grupa.getPromovabilitate(), 0.01);
+	}
+	
+	
+}
